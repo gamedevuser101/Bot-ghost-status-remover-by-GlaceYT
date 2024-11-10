532d09bd4df7d7cfbb31709a3780e1bb7acf3d10
gamedevuser101 <146693756+gamedevuser101@users.noreply.github.com>
2024-11-10 08:12:23 -0800

    Update index.jss

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["The Best Security Bot"];
+const statusMessages = ["The Best Security Bot" "Vote At https://top.gg/bot/1290502191420801077"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
